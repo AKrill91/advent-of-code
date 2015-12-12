2181de926d49c4e08db4008fe0cd21333a6e880a
Austin Krill <akrill91@gmail.com>
2015-12-11 19:51:24 -0500

    Solver for Day 01.

--- src/main/java/com/stexnistudios/adventofcode/Application.java
+++ src/main/java/com/stexnistudios/adventofcode/Application.java
@@ -1,7 +1,30 @@
 package com.stexnistudios.adventofcode;
 
-public class Application {
-    public static void main(String[] args) {
+import com.google.common.base.Strings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+
+public class Application {
+    public static void main(String[] args) throws IOException {
+        Logger logger = LoggerFactory.getLogger(Application.class);
+
+        int day = Integer.parseInt(args[0]);
+        String dayStr = Strings.padStart(String.valueOf(day), 2, '0');
+
+        logger.info("Running solver for day {}", dayStr);
+
+        Solver solver = null;
+
+        switch (day) {
+            case 1:
+                solver = new Day01Solver(dayStr);
+                break;
+        }
+
+        if(solver != null) {
+            solver.run();
+        }
     }
 }
---
+++ src/main/java/com/stexnistudios/adventofcode/Solver.java
@@ -0,0 +1,49 @@
+package com.stexnistudios.adventofcode;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public abstract class Solver implements Runnable {
+
+    protected final List<String> input;
+    protected final Logger logger;
+
+    public Solver(List<String> input) {
+        this.input = Collections.unmodifiableList(input);
+        this.logger = LoggerFactory.getLogger(this.getClass());
+    }
+
+    public Solver(String fileName) throws IOException {
+        this(loadInputFromFile(fileName));
+    }
+
+    private static List<String> loadInputFromFile(String fileName) throws IOException {
+        Logger logger = LoggerFactory.getLogger(Solver.class);
+        List<String> lines = new ArrayList<>();
+
+        try (
+            BufferedReader reader = new BufferedReader(
+                new InputStreamReader(
+                    Thread.currentThread()
+                        .getContextClassLoader()
+                        .getResourceAsStream("day01.txt")
+                )
+            )
+        ) {
+            String line = "";
+
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+        }
+
+        return lines;
+    }
+}
