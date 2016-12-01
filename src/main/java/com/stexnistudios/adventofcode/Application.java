c75449ec37a58749160153018850e89174dd541f
Austin Krill <austin.krill@forrent.com>
2016-12-01 07:42:29 -0500

    Setup for 2016.

--- src/main/java/com/stexnistudios/adventofcode/Application.java
+++ src/main/java/com/stexnistudios/adventofcode/Application.java
@@ -6,61 +6,20 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
-public class Application {
+public class Application implements Runnable {
     public static void main(String[] args) throws IOException {
-        Logger logger = LoggerFactory.getLogger(Application.class);
+        new Application().run();
+    }
 
-        int day = 0;
-        if (args.length > 0) {
-            day = Integer.parseInt(args[0]);
-        }
+    private final Logger logger;
 
-        String dayStr = Strings.padStart(String.valueOf(day), 2, '0');
-        Runnable solver = null;
-        String fileName = "day" + dayStr + ".txt";
+    public Application() {
+        logger = LoggerFactory.getLogger(getClass());
+    }
 
-        switch (day) {
-            case 1:
-                solver = new Day01Solver(fileName);
-                break;
-            case 2:
-                solver = new Day02Solver(fileName);
-                break;
-            case 3:
-                solver = new MultiSolver(
-                    new Day03Solver(fileName),
-                    new Day03Solver(fileName, 2)
-                );
-                break;
-            case 6:
-                solver = new Day06Solver(fileName);
-                break;
-            case 7:
-                solver = new Day07Solver(fileName);
-                break;
-            case 8:
-                solver = new Day08Solver(fileName);
-                break;
-            case 9:
-                solver = new Day09Solver(fileName);
-                break;
-            case 10:
-                solver = new Day10Solver(fileName);
-                break;
-            case 13:
-                solver = new Day13Solver(fileName);
-                break;
-            case 14:
-                solver = new Day14Solver(fileName);
-                break;
-            default:
-                logger.info("No solver found for day {}", dayStr);
-                break;
-        }
 
-        if (solver != null) {
-            logger.info("Running solver for day {}", dayStr);
-            solver.run();
-        }
+    @Override
+    public void run() {
+        logger.info("Hello, World!");
     }
 }
