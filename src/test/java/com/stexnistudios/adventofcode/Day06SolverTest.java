103e9f46b23c3c4fb5d9ab1d7b3c13b13837d74f
Austin Krill <austin.krill@forrent.com>
2016-12-06 08:09:05 -0500

    Day06a solved.

---
+++ src/test/java/com/stexnistudios/adventofcode/Day06SolverTest.java
@@ -0,0 +1,30 @@
+package com.stexnistudios.adventofcode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Day06SolverTest {
+    @Test
+    public void testA() {
+        String input = "eedadn\n" +
+            "drvtee\n" +
+            "eandsr\n" +
+            "raavrd\n" +
+            "atevrs\n" +
+            "tsrnev\n" +
+            "sdttsa\n" +
+            "rasrtv\n" +
+            "nssdts\n" +
+            "ntnada\n" +
+            "svetve\n" +
+            "tesnvt\n" +
+            "vntsnd\n" +
+            "vrdear\n" +
+            "dvrsen\n" +
+            "enarar";
+
+        Day06aSolver solver = new Day06aSolver(input);
+        Assert.assertEquals("easter", solver.call());
+    }
+
+}
