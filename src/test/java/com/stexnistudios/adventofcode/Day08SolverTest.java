4a05a888fe2f3c3ddf24caa882dacb4358d07a9f
Austin Krill <austin.krill@forrent.com>
2016-12-08 09:00:00 -0500

    Day08a solved.

---
+++ src/test/java/com/stexnistudios/adventofcode/Day08SolverTest.java
@@ -0,0 +1,18 @@
+package com.stexnistudios.adventofcode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Day08SolverTest {
+    @Test
+    public void testA() throws Exception {
+        String input = "rect 3x2\n" +
+            "rotate column x=1 by 1\n" +
+            "rotate row y=0 by 4\n" +
+            "rotate column x=1 by 1";
+
+        Day08aSolver solver = new Day08aSolver(input, 7, 3);
+
+        Assert.assertEquals(6, solver.call().intValue());
+    }
+}
