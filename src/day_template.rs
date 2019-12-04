4074ca1fce109b0e34576904e63fa7836e41692b
Austin Krill <akrill@costar.com>
2019-12-04 07:53:18 -0500

    Add a template

---
+++ src/day_template.rs
@@ -0,0 +1,16 @@
+pub fn run_a(input: &Vec<String>) -> i64{
+    0
+}
+
+pub fn run_b(input: &Vec<String>) -> i64 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    pub fn sample_input_0_a() {
+    }
+}
