4b3292377ca00c4779f0fbb0da2d5666331cc28c
Austin Krill <akrill@costar.com>
2021-12-16 16:36:10 -0500

    Add more to day template

--- src/year2021/day_template.rs
+++ src/year2021/day_template.rs
@@ -15,14 +15,29 @@ mod test {
         let _ = env_logger::builder().is_test(true).try_init();
     }
 
+    fn get_sample() -> Vec<String> {
+        vec![]
+    }
 
     #[test]
     fn test_sample_a() {
         init();
+
+        let input = get_sample();
+
+        let expected = "";
+
+        assert_eq!(expected, run_a(0, &input));
     }
 
     #[test]
     fn test_sample_b() {
         init();
+
+        let input = get_sample();
+
+        let expected = "";
+
+        assert_eq!(expected, run_b(0, &input));
     }
 }
