d2eddf74e213f15912eabc2811b601f05bb97c79
Austin Krill <akrill91@gmail.com>
2022-12-03 20:38:00 -0500

    2022: Day01 in terraform

---
+++ src/2022/day01.tf
@@ -0,0 +1,47 @@
+data "local_file" "day01_input" {
+  filename = "${path.module}/../../resources/2022/day01.txt"
+}
+
+locals {
+  calorie_groups = split("\n\n", data.local_file.day01_input.content)
+  elf_calories_str = [
+    for elf in local.calorie_groups:
+      compact(split("\n", elf))
+  ]
+
+  elf_calories = [
+    for elf in local.elf_calories_str:
+      [
+        for calorie in elf:
+          tonumber(calorie)
+      ]
+  ]
+
+  elf_calorie_sums = [
+    for elf in local.elf_calories:
+      sum(elf)
+  ]
+  elf_calorie_sums_padded = [
+    for n in local.elf_calorie_sums:
+      format("%05d", n)
+  ]
+  elf_calorie_sums_sorted = [
+    for n in reverse(sort(local.elf_calorie_sums_padded)):
+      tonumber(n)
+  ]
+}
+
+output "debug" {
+  value = {
+#    calorie_groups = local.calorie_groups
+#    elf_calories_str = local.elf_calories_str
+    slice = slice(local.elf_calorie_sums_sorted, 0, 3)
+  }
+}
+
+output "day01" {
+  value = {
+    a = max(local.elf_calorie_sums...)
+    b = sum(slice(local.elf_calorie_sums_sorted, 0, 3))
+  }
+}
