3d533ba612c2b46a9bf597461aae405787654be0
Austin Krill <akrill91@gmail.com>
2024-12-01 16:04:19 -0500

    2024 Day01b

--- src/day01.rs
+++ src/day01.rs
@@ -1,7 +1,7 @@
 use log::debug;
 use regex::Regex;
 
-pub fn run_a(input: &str) -> i64 {
+fn parse(input: &str) -> (Vec<i64>, Vec<i64>) {
     let input = input.trim();
     let mut left = vec![];
     let mut right = vec![];
@@ -20,6 +20,12 @@ pub fn run_a(input: &str) -> i64 {
     left.sort();
     right.sort();
 
+    (left, right)
+}
+
+pub fn run_a(input: &str) -> i64 {
+    let (left, right) = parse(input);
+
     let mut total_distance = 0;
 
     for i in 0..left.len() {
@@ -29,29 +35,49 @@ pub fn run_a(input: &str) -> i64 {
     total_distance
 }
 
+pub fn run_b(input: &str) -> i64 {
+    let (left, right) = parse(input);
+
+    let mut similarity_score = 0;
+
+    for l in left {
+        let num_occurences = right.iter().filter(|x| **x == l).count();
+        similarity_score += l * num_occurences as i64;
+    }
+
+    similarity_score
+}
+
 #[cfg(test)]
 mod test {
-    use crate::day01::run_a;
 
     fn init() {
         let _ = env_logger::builder().is_test(true).try_init();
     }
 
-    #[test]
-    fn part_a_example() {
-        init();
-
-        let input = r"
+    fn input() -> &'static str {
+        r"
 3   4
 4   3
 2   5
 1   3
 3   9
 3   3
-";
+"
+    }
 
-        let distance = run_a(input);
+    #[test]
+    fn part_a_example() {
+        init();
+
+        let distance = super::run_a(input());
 
         assert_eq!(distance, 11);
+    }
+
+    #[test]
+    fn part_b_example() {
+        let score = super::run_b(input());
+        assert_eq!(score, 31)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,12 @@ fn main() {
 
     let start = Instant::now();
 
-    let day01_a = day01::run_a(std::fs::read_to_string("resources/2024/day01.txt").unwrap().as_str());
+    let day01_input = std::fs::read_to_string("resources/2024/day01.txt").unwrap();
 
+    let day01_a = day01::run_a(&day01_input);
     info!("Day 01 part a = {}", day01_a);
+    let day01_b = day01::run_b(&day01_input);
+    info!("Day 01 part b = {}", day01_b);
 
     info!("Overall - took {:?}", start.elapsed());
 }
