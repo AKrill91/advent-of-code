35d3e423ed550e551a5d15e70ff8633d1dd7a46c
Austin Krill <akrill@costar.com>
2024-12-03 13:48:21 -0500

    2024 Day03

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-mod day01;
-mod day02;
+mod day03;
 
 use std::time::Instant;
 use log::info;
@@ -9,12 +8,12 @@ fn main() {
 
     let start = Instant::now();
 
-    let day02_input = std::fs::read_to_string("resources/2024/day02.txt").unwrap();
+    let day03_input = std::fs::read_to_string("resources/2024/day03.txt").unwrap();
 
-    let day02_a = day02::run_a(&day02_input);
-    info!("Day 02 part a = {}", day02_a);
-    let day02_b = day02::run_b(&day02_input);
-    info!("Day 02 part b = {}", day02_b);
+    let day03_a = day03::run_a(&day03_input);
+    info!("Day 02 part a = {}", day03_a);
+    let day03_b = day03::run_b(&day03_input);
+    info!("Day 02 part b = {}", day03_b);
 
     info!("Overall - took {:?}", start.elapsed());
 }
