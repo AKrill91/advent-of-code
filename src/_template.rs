0c68dc37561026e8559ba18b98efae48461ca4da
Austin Krill <akrill91@gmail.com>
2024-12-07 15:55:38 -0500

    2024 Day07

---
+++ src/_template.rs
@@ -0,0 +1,31 @@
+
+pub fn run_a(input: &str) -> i64 {
+    0
+}
+
+pub fn run_b(input: &str) -> i64 {
+    0
+}
+
+#[cfg(test)]
+mod test {
+    fn init() {
+        let _ = env_logger::builder().is_test(true).try_init();
+    }
+
+    fn example() -> &'static str {
+        r""
+    }
+
+
+    #[test]
+    fn part_a_example() {
+        init();
+        assert_eq!(1, super::run_a(example()));
+    }
+
+    #[test]
+    fn part_b_example() {
+        assert_eq!(1, super::run_b(example()));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-mod day06;
+mod day07;
 mod utils;
 
 use std::time::Instant;
@@ -8,15 +8,15 @@ fn main() {
     env_logger::init();
 
     let start = Instant::now();
-    let day06_input = std::fs::read_to_string("resources/2024/day06.txt").unwrap();
+    let day07_input = std::fs::read_to_string("resources/2024/day07.txt").unwrap();
 
     let a_start = Instant::now();
-    let day06_a = day06::run_a(&day06_input);
-    info!("Day 06 part a = {}, took {:?}", day06_a, a_start.elapsed());
+    let day07_a = day07::run_a(&day07_input);
+    info!("Day 07 part a = {} , took {:?}", day07_a, a_start.elapsed());
 
     let b_start = Instant::now();
-    let day06_b = day06::run_b(&day06_input);
-    info!("Day 06 part b = {}, took {:?}", day06_b, b_start.elapsed());
+    let day07_b = day07::run_b(&day07_input);
+    info!("Day 07 part b = {} , took {:?}", day07_b, b_start.elapsed());
 
     info!("Overall - took {:?}", start.elapsed());
 }
